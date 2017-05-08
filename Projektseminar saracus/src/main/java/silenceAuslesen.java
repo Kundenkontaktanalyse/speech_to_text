6dae160160bce9820eda51c2001fa69b85bdea77
a_orsc01 <a.orschel@gmx.de>
2017-05-08 15:33:43 +0200

    Test

--- Projektseminar saracus/src/main/java/silenceAuslesen.java
+++ Projektseminar saracus/src/main/java/silenceAuslesen.java
@@ -1,31 +1,41 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class silenceAuslesen {
 	
 	static String fileName = "channel1.wav"; // Variable für Dateinamen
+	final static String VERZEICHNIS = ("C:\\Users\\a_orsc01\\Desktop\\newfile.txt\");
 	
     public static void main(String[] args) throws Exception {
-  
-    	//ProcessBuilder Objekt erstellen
-        ProcessBuilder silenceAusleser = new ProcessBuilder(
-        		
-        	
-        		"cmd.exe", "/c", //CMD Befehl
-        		"cd \"C:\\Users\\a_orsc01\\Desktop\\ffmpeg\\bin\" &&" //Pfad für Programm
-        		+ "ffmpeg " + "-i " + fileName + " -af silencedetect=noise=-50dB:d=1 -f null -"); //Befehl
-        
-       // TODO silence mit
-         //hallo
-        silenceAusleser.redirectErrorStream(true);
-        Process p = silenceAusleser.start();
-        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line;
-        while (true) {
-            line = r.readLine();
-            if (line == null) { break; }
-            System.out.println(line);
-        }
-    }
+    	
+    	 
+              try
+              {
+                  BufferedReader bReader = new BufferedReader(new FileReader(VERZEICHNIS));
+                  
+                  String Hallo = null;
+                  String Zeile = bReader.readLine();
+                  while (true)
+                  {
+                      Zeile = bReader.readLine();
+                      if (Zeile.contains("silence_start"))
+                      {
+                    	  Hallo = Zeile;
+                      }  
+                       
+                      if (Zeile == null) { break; }
+                      
+                      System.out.println(Hallo);
+                     
+                  }
+              }
+              catch(IOException ioe)
+              {
+                  ioe.printStackTrace();
+              }
+          
+    }}
     
-}
+
