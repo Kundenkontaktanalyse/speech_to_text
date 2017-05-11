c33f2ebeb1723f1029fa90f3db74db8988957158
Alex <a_orsc01@uni-muenster.de>
2017-05-11 15:24:00 +0200

    Änderung

--- Projektseminar saracus/src/main/java/silenceAuslesen.java
+++ Projektseminar saracus/src/main/java/silenceAuslesen.java
@@ -6,28 +6,29 @@ import java.io.FileWriter;
 
 public class silenceAuslesen {
 
-	static String fileName = "channel1.wav"; // Variable für Dateinamen
 	final static String VERZEICHNIS = ("C:\\Users\\Alex\\Desktop\\newfile.txt");
+	
 
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader bReader = new BufferedReader(new FileReader(VERZEICHNIS));
 		BufferedWriter out = new BufferedWriter(new FileWriter("C:\\Users\\Alex\\Desktop\\newfile2.txt"));
 		
-		
-		
-		
-		
 		while (true) {
 			String Zeile = bReader.readLine();
 			if (Zeile == null) { break; }
 			if (Zeile.contains("silence_end")) {
-				out.write(Zeile);
-				
+				int indexDoppelpunkt = Zeile.indexOf(":") + 1; 
+				int indexSonderzeichen = Zeile.indexOf("|") - 1;
+				Zeile = Zeile.substring(indexDoppelpunkt,indexSonderzeichen);
+				Zeile.replaceAll("[^.0-9"+ "]", "");
+				out.write(Zeile);	
 			}
-			
 		}
+		
+
 		out.close();
 		bReader.close();
+		
 	}
 }
