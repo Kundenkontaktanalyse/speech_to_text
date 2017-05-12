93e278b93343c5a30bd67acfc86b1bcc6741d8f3
a_orsc01 <a.orschel@gmx.de>
2017-05-12 17:10:07 +0200

    Final

---
+++ Projektseminar saracus/src/main/java/TimesCut.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TimesCut extends FileChooser {
+
+	public void logic(File output) throws IOException {
+
+		File file = choose();
+
+		BufferedReader bReader = new BufferedReader(new FileReader(file));
+		BufferedWriter out = new BufferedWriter(new FileWriter(output));
+
+		while (true) {
+			String Zeile = bReader.readLine();
+			if (Zeile == null) {
+				break;
+			}
+			if (Zeile.contains("silence_end")) {
+				int indexDoppelpunkt = Zeile.indexOf(":") + 1;
+				int indexSonderzeichen = Zeile.indexOf("|") - 1;
+				Zeile = Zeile.substring(indexDoppelpunkt, indexSonderzeichen);
+				Zeile.replaceAll("[^.0-9" + "]", "");
+				out.write(Zeile);
+			}
+		}
+		out.close();
+		bReader.close();
+
+	}
+
+	public static void main(String[] args) {
+
+		File catext = new File("C:\\Users\\a_orsc01\\Desktop\\catimes.txt");
+		File kuntext = new File("C:\\Users\\a_orsc01\\Desktop\\kuntimes.txt");
+		
+		TimesCut mytest = new TimesCut();
+
+		try {
+			mytest.logic(catext);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			mytest.logic(kuntext);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
