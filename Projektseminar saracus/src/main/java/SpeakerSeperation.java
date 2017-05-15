32803c31c82fdde08418d312cad24e84ab444614
d_tham01 <d_tham01@D-3102W22.WIWI.UNI-MUENSTER.DE>
2017-05-15 15:17:07 +0200

    cutting methode unfertig

--- Projektseminar saracus/src/main/java/SpeakerSeperation.java
+++ Projektseminar saracus/src/main/java/SpeakerSeperation.java
@@ -38,6 +38,7 @@ import java.util.Arrays;
  *  - Input benennen, sodass 10ter schnitt keine stellenverschiebung generiert
  *  
  *  - + 99999 ausbessern mit "still-to-go" variable
+ *  - cut methode schreiben
  */
 
 public class SpeakerSeperation extends AudioProcessing {
@@ -50,6 +51,8 @@ public class SpeakerSeperation extends AudioProcessing {
 	File[] textfiles;
 	double[] startzeitenKU;
 	double[] startzeitenCA;
+	double[] startzeitenKUrdy;
+	double[] startzeitenCArdy;
 
 	/*
 	 * Methode zum Verarbeiten der Input-Dateien: Dateien werden nach Audio und
@@ -92,12 +95,12 @@ public class SpeakerSeperation extends AudioProcessing {
 		Arrays.sort(textfiles); // alphabetische Sortierung scheint automatisch
 								// zu erfolgen, wegen hinweis der API nochmal
 								// zur Sicherheit
-//		if (textfiles != null) { // Erforderliche Berechtigungen etc. sind
-									// vorhanden
-			// for (int i = 0; i < textfiles.length; i++) {
-			// System.out.println(textfiles[i].getAbsolutePath());
-			// }
-//		}
+		// if (textfiles != null) { // Erforderliche Berechtigungen etc. sind
+		// vorhanden
+		// for (int i = 0; i < textfiles.length; i++) {
+		// System.out.println(textfiles[i].getAbsolutePath());
+		// }
+		// }
 	}
 
 	// Methode zum Umwandeln einer Textdatei in ein Int[]
@@ -121,39 +124,41 @@ public class SpeakerSeperation extends AudioProcessing {
 		}
 		// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 		// ALTE VESION: Zeilenweises Auslesen der Zeiten
-//		int count = 0;
-//		try {
-//			while (br.readLine() != null) {
-//				count++;
-//			}
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		// System.out.println("Anzahl: " + count);
-//		zeiten = new int[count];
+		// int count = 0;
+		// try {
+		// while (br.readLine() != null) {
+		// count++;
+		// }
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		// // System.out.println("Anzahl: " + count);
+		// zeiten = new int[count];
 		// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-		
 
-		// InputString bei Leerzeichen trennen ACHTUNG: Ein String mehr als Zeiten, da Trennung "vor" erstem String
+		// InputString bei Leerzeichen trennen ACHTUNG: Ein String mehr als
+		// Zeiten, da Trennung "vor" erstem String
 		String[] StringZeiten = inputString.split("\\s+");
 
-//		for (int i = 0; i < StringZeiten.length; i++) {
-//			System.out.println("ziffernString = " + StringZeiten[i]);
-//		}
+		// for (int i = 0; i < StringZeiten.length; i++) {
+		// System.out.println("ziffernString = " + StringZeiten[i]);
+		// }
 
-		// Strings in Integer parsen (stringArray besitzt als ersten string nur leerzeichen durch trennung, daher indexverschiebung)
+		// Strings in Integer parsen (stringArray besitzt als ersten string nur
+		// leerzeichen durch trennung, daher indexverschiebung)
 		zeiten = new double[StringZeiten.length - 1];
 		for (int i = 1; i < StringZeiten.length; i++) {
-			zeiten[i-1] = Double.parseDouble(StringZeiten[i]);
+			zeiten[i - 1] = Double.parseDouble(StringZeiten[i]);
 		}
 
-//		for (int i = 0; i < zeiten.length; i++) {
-//			System.out.println("doublewert = " + zeiten[i]);
-//		}
+		// for (int i = 0; i < zeiten.length; i++) {
+		// System.out.println("doublewert = " + zeiten[i]);
+		// }
 
-//		XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+		// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 		// Abschnitt zum Schreiben der Zeiten in ein Int[], erneutes Einlesen
-		// der Datei da Marker nach Durchlauf am Ende der Datei (Alte Versoin: Textdatei Zeilenweise einlesen)
+		// der Datei da Marker nach Durchlauf am Ende der Datei (Alte Versoin:
+		// Textdatei Zeilenweise einlesen)
 
 		// try {
 		// br = new BufferedReader(new InputStreamReader(new
@@ -174,7 +179,7 @@ public class SpeakerSeperation extends AudioProcessing {
 		// // System.out.println(zeiten[i]);
 		// // }
 		// return zeiten;
-//		XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+		// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 		return zeiten;
 	}
 
@@ -182,27 +187,58 @@ public class SpeakerSeperation extends AudioProcessing {
 	// StartzeitenArrays
 	public void initalizeData() {
 		getDataToArrays();
-		startzeitenKU = TextToTime(textfiles[0]);
-		startzeitenCA = TextToTime(textfiles[1]);
+		startzeitenCA = TextToTime(textfiles[0]);
+		startzeitenKU = TextToTime(textfiles[1]);
+		identifyStarter();
+		
 //		 System.out.println(textfiles[0]);
 //		 System.out.println(textfiles[1]);
 //		 System.out.println("---------------");
-//		 System.out.println(startzeitenKU.length);
-//		 System.out.println(startzeitenCA.length);
-//		 for(int i = 0; i<startzeitenKU.length;i++){
-//		 System.out.println(startzeitenKU[i]);
+//		 System.out.println(startzeitenKUrdy.length);
+//		 System.out.println(startzeitenCArdy.length);
+//		 for(int i = 0; i<startzeitenKUrdy.length;i++){
+//		 System.out.println(startzeitenKUrdy[i]);
 //		 }
 //		 System.out.println();
-//		 for(int i = 0; i<startzeitenCA.length;i++){
-//		 System.out.println(startzeitenCA[i]);
+//		 for(int i = 0; i<startzeitenCArdy.length;i++){
+//		 System.out.println(startzeitenCArdy[i]);
 //		 }
-		splitSpeaker();
+		
+		 splitSpeaker();
+	}
+
+	// Methode zur Anpassung der double[] startzeiten: Einfügen einer 0.0 beim
+	// Beginner des Gesprächs
+	public void identifyStarter() {
+
+		if (startzeitenCA[0] < startzeitenKU[0]) {
+
+			startzeitenCArdy = startzeitenCA;
+
+			startzeitenKUrdy = new double[startzeitenKU.length + 1];
+			startzeitenKUrdy[0] = 0.0;
+			for (int i = 1; i < startzeitenKUrdy.length; i++) {
+				startzeitenKUrdy[i] = startzeitenKU[i - 1];
+
+			}
+
+		} else {
+			startzeitenKUrdy = startzeitenKU;
+
+			startzeitenCArdy = new double[startzeitenCA.length + 1];
+			startzeitenCArdy[0] = 0.0;
+			for (int i = 1; i < startzeitenCArdy.length; i++) {
+				startzeitenCArdy[i] = startzeitenCA[i - 1];
+
+			}
+
+		}
 	}
 
 	public void splitSpeaker() {
 
-		audiofilesKU = new File[startzeitenKU.length];
-		audiofilesCA = new File[startzeitenCA.length];
+		audiofilesKU = new File[startzeitenKUrdy.length];
+		audiofilesCA = new File[startzeitenCArdy.length];
 		int insertIntoKU = 0;
 		int insertIntoCA = 0;
 
@@ -211,13 +247,13 @@ public class SpeakerSeperation extends AudioProcessing {
 			// Kunde, 2.1.wav -> Speaker Agent
 			if (audiofiles[i].toString()
 					.substring(audiofiles[i].toString().length() - 7, audiofiles[i].toString().length() - 6)
-					.equals("1")) {
+					.equals("K")) {
 				audiofilesKU[insertIntoKU] = audiofiles[i];
 				insertIntoKU++;
 			} else {
 				if (audiofiles[i].toString()
 						.substring(audiofiles[i].toString().length() - 7, audiofiles[i].toString().length() - 6)
-						.equals("2")) {
+						.equals("C")) {
 					audiofilesCA[insertIntoCA] = audiofiles[i];
 					insertIntoCA++;
 				} else {
@@ -226,23 +262,23 @@ public class SpeakerSeperation extends AudioProcessing {
 			}
 		}
 
-		// for (int i = 0; i < audiofilesKU.length; i++) {
-		// System.out.print(audiofilesKU[i].getAbsolutePath());
-		// if (audiofilesKU[i].isDirectory()) {
-		// System.out.print(" (Ordner)\n");
-		// } else {
-		// System.out.print(" (Datei)\n");
-		// }
-		// }
-		// System.out.println("-------------------");
-		// for (int i = 0; i < audiofilesCA.length; i++) {
-		// System.out.print(audiofilesCA[i].getAbsolutePath());
-		// if (audiofilesCA[i].isDirectory()) {
-		// System.out.print(" (Ordner)\n");
-		// } else {
-		// System.out.print(" (Datei)\n");
-		// }
-		// }
+//		 for (int i = 0; i < audiofilesKU.length; i++) {
+//		 System.out.print(audiofilesKU[i].getAbsolutePath());
+//		 if (audiofilesKU[i].isDirectory()) {
+//		 System.out.print(" (Ordner)\n");
+//		 } else {
+//		 System.out.print(" (Datei)\n");
+//		 }
+//		 }
+//		 System.out.println("-------------------");
+//		 for (int i = 0; i < audiofilesCA.length; i++) {
+//		 System.out.print(audiofilesCA[i].getAbsolutePath());
+//		 if (audiofilesCA[i].isDirectory()) {
+//		 System.out.print(" (Ordner)\n");
+//		 } else {
+//		 System.out.print(" (Datei)\n");
+//		 }
+//		 }
 
 	}
 
@@ -266,34 +302,37 @@ public class SpeakerSeperation extends AudioProcessing {
 		// Idee eines Stack:
 		// Zeiger auf jeweiligen Feldern, "abnehmen" des aktuellsten Elemts
 		// beider felder
-		
-		//Problem der Indizes: Falls ein Stack leer ist / der pointer eines Felds am Ende steht, vergleicht das Element des anderes Felds
-		// gegen Null(hinter das andere durch ++). Daher bisher unsauber: Falls letztes Element erreicht wurde startzeit des letzten Elements verändern sodass diese
+
+		// Problem der Indizes: Falls ein Stack leer ist / der pointer eines
+		// Felds am Ende steht, vergleicht das Element des anderes Felds
+		// gegen Null(hinter das andere durch ++). Daher bisher unsauber: Falls
+		// letztes Element erreicht wurde startzeit des letzten Elements
+		// verändern sodass diese
 		// auf jeden fall höher als das andere Element ist
 		// TODO : Anpassen
-		
+
 		int currentPositionKU = 0;
 		int currentPositionCA = 0;
-//test-komment git
+
 		for (int i = 0; i < audiofiles.length; i++) {
-			if (startzeitenKU[currentPositionKU] < startzeitenCA[currentPositionCA]) {
+			if (startzeitenKUrdy[currentPositionKU] < startzeitenCArdy[currentPositionCA]) {
 				bundler.addTextSync("\n Kunde:  " + currentPositionKU);
 				processAudio(audiofilesKU[currentPositionKU]);
 
-				if (currentPositionKU < startzeitenKU.length - 1) {
+				if (currentPositionKU < startzeitenKUrdy.length - 1) {
 					currentPositionKU++;
 				} else {
-					startzeitenKU[currentPositionKU] = startzeitenCA[currentPositionCA] + 99999;
+					startzeitenKUrdy[currentPositionKU] = startzeitenCArdy[currentPositionCA] + 99999;
 				}
 			} else {
-				if (startzeitenCA[currentPositionCA] < startzeitenKU[currentPositionKU])
+				if (startzeitenCArdy[currentPositionCA] < startzeitenKUrdy[currentPositionKU])
 					bundler.addTextSync("\n Agent:  " + currentPositionCA);
 				processAudio(audiofilesCA[currentPositionCA]);
-				
-				if (currentPositionCA < startzeitenCA.length - 1) {
+
+				if (currentPositionCA < startzeitenCArdy.length - 1) {
 					currentPositionCA++;
 				} else {
-					startzeitenCA[currentPositionCA] = startzeitenKU[currentPositionKU] + 99999;
+					startzeitenCArdy[currentPositionCA] = startzeitenKUrdy[currentPositionKU] + 99999;
 				}
 
 			}
--- Projektseminar saracus/src/main/java/test.java
+++ Projektseminar saracus/src/main/java/test.java
@@ -1,87 +1,152 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 
-public class test {
+public class test extends FileChooser {
 
-	public int[] TextToTime(File startingtimes) {
+	File audioCA = choose();
+	File audioKU = choose();
 
-		int[] zeiten;
-		String inputString = null;
+	String audioCAFilePath = audioCA.getPath().toString();
+	String audioKUFilePath = audioKU.getPath().toString();
+	String destinationCAFilePath = audioCAFilePath;
+	String destinationKUFilePath = audioKUFilePath;
 
-		// Abschnitt zur Bestimmung der Anzahl der Startzeiten
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(startingtimes)));
+	AudioInputStream inputStreamCA = null;
+	AudioInputStream shortenedStreamCA = null;
+	AudioInputStream inputStreamKU = null;
+	AudioInputStream shortenedStreamKU = null;
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+	long framesOfAudioToCopy;
+	int cutAnzahlCA;
+	int cutAnzahlKU;
+
+	double[] secondsToCopyCA;
+	double[] secondsToSkipCA;
+	double[] secondsToCopyKU;
+	double[] secondsToSkipKU;
+
+	public void audioCutting(double[] startzeitenCALL, double[] startzeitenKUNDE) {
+
+		// bestimmung der länge des letzten teils durch subtraktion von
+		// gesamtlänge des audio
+		// daher warnung falls channelgröße mehr als 5000 byte Unterschied
+		if (Math.abs(audioCA.length() - audioKU.length()) < 5000) {
+			System.out.println("Channels Unterschiedlich lang");
 		}
+
+		cutAnzahlCA = startzeitenCALL.length;
+		cutAnzahlKU = startzeitenKUNDE.length;
+
+		int secondsToCopy2 = 50;
+
 		try {
-			inputString = br.readLine();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+			File file = new File(audioCAFilePath);
+			AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
+			AudioFormat format = fileFormat.getFormat();
+
+			int bytesPerSecond = format.getFrameSize() * (int) format.getFrameRate(); // hz*bytes/frames
+			double audioFileLength = file.length();
+			double audioSekunden = audioFileLength / bytesPerSecond;
+
+			inputStream = AudioSystem.getAudioInputStream(file); // eine ID
+			double audioFileLength = file.length(); // get Length des inputs
+
+			int bytesPerSecond = format.getFrameSize() * (int) format.getFrameRate(); // hz*bytes/frames
+
+			String origdestinationFilePath = destinationFilePath;
+
+			// Gedankenfalle skip:
+			// Vermutung: Durch die Erstellung des shortenedStream wird intern
+			// ein Marker weitergesetzt, der das skip unnötig werden lässt
+			// ansonsten zweimaliges Überspringen mit lücken
+
+			for (int i = 1; i <= cutAnzahl; i++) {
+
+				framesOfAudioToCopy = secondsToCopy * (int) format.getFrameRate();
+				// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+				destinationFilePath = origdestinationFilePath;
+
+				shortenedStream = new AudioInputStream(inputStream, format, framesOfAudioToCopy);
+
+				// bisher nur .wav, später flexibel gestalten evt.
+				int pfadlaenge = destinationFilePath.length();
+
+				String insert = null;
+				if (i < 10) {
+					insert = "_0";
+				} else {
+					insert = "_";
+				}
+				destinationFilePath = destinationFilePath.substring(0, pfadlaenge - 4) + insert + i
+						+ destinationFilePath.substring(pfadlaenge - 4, pfadlaenge);
+
+				File destinationFile = new File(destinationFilePath);
+				AudioSystem.write(shortenedStream, fileFormat.getType(), destinationFile);
+
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if (inputStream != null)
+				try {
+					inputStream.close();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			if (shortenedStream != null)
+				try {
+					shortenedStream.close();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
 		}
-		// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-		// ALTE VESION: Zeilenweises Auslesen der Zeiten
-//		int count = 0;
-//		try {
-//			while (br.readLine() != null) {
-//				count++;
-//			}
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		// System.out.println("Anzahl: " + count);
-//		zeiten = new int[count];
-		// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+	}
+
+	
+	
+	/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	 * NONESENCE!!!!!!!!!!! Startzeiten der Stille aus ffmpeg lesen, nicht aus arrays ermitteln.
+	 * format am besten: startzeiten in einer zeile.
+	 * format faken bis ffmpeg angepasst
+	 * 
+	 * 
+	 * Methode bestimmung der Audiozeiten in denen geredet wird auf beiden channeln
+	 * ändert:	double[] secondsToCopyCA;
+				double[] secondsToSkipCA;
+				double[] secondsToCopyKU;
+				double[] secondsToSkipKU;
+	 * 	
+	 * XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	 */
+	public void startzeitenToSkipAndCopy(double[] startzeitenCALL, double[] startzeitenKUNDE) {
+		
+		// Unterscheidung des Beginners: Gesprächsteillänge des zweiten Redners durch Vegleich mit nächstem Wert des anderen
+		if(startzeitenKUNDE[0] == 0.0){
+			
+		for (int i = 0; i < Math.max(startzeitenCALL.length, startzeitenKUNDE.length ); i++) {
+			secondsToCopyKU[i] =  startzeitenCALL[i] - startzeitenKUNDE[i];
+			secondsToCopyCA[i] = startzeitenKUNDE[i+1] - startzeitenCALL[i];
+		}
+		} else {
+			
+			for (int i = 0; i < Math.max(startzeitenCALL.length, startzeitenKUNDE.length ); i++) {
+				secondsToCopyKU[i] =  startzeitenCALL[i+1] - startzeitenKUNDE[i];
+				secondsToCopyCA[i] = startzeitenKUNDE[i] - startzeitenCALL[i];
+			}
+			
+		}
+		// Werte zum herausschneiden der Stille um geringere bearbeitungszeit der API zu erhalten; genausolang wie die eine Spur
+		// redet muss die andere übersprungen
+		secondsToSkipCA = secondsToCopyKU;
+		secondsToSkipKU = secondsToCopyCA;
 		
 
-		// InputString bei Leerzeichen trennen ACHTUNG: Ein String mehr als Zeiten, da Trennung "vor" erstem String
-		String[] StringZeiten = inputString.split("\\s+");
-
-//		for (int i = 0; i < StringZeiten.length; i++) {
-//			System.out.println("ziffernString = " + StringZeiten[i]);
-//		}
-
-		// Strings in Integer parsen (stringArray besitzt als ersten string nur leerzeichen durch trennung, daher indexverschiebung)
-		zeiten = new int[StringZeiten.length - 1];
-		for (int i = 1; i < StringZeiten.length; i++) {
-			zeiten[i-1] = Integer.parseInt(StringZeiten[i]);
-		}
-
-//		for (int i = 0; i < zeiten.length; i++) {
-//			System.out.println("integerwert = " + zeiten[i]);
-//		}
-
-//		XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-		// Abschnitt zum Schreiben der Zeiten in ein Int[], erneutes Einlesen
-		// der Datei da Marker nach Durchlauf am Ende der Datei (Alte Versoin: Textdatei Zeilenweise einlesen)
-
-		// try {
-		// br = new BufferedReader(new InputStreamReader(new
-		// FileInputStream(startingtimes)));
-		// } catch (FileNotFoundException e) {
-		// e.printStackTrace();
-		// }
-		//
-		// for (int i = 0; i < anzahlZeiten; i++) {
-		// try {
-		// zeiten[i] = Integer.parseInt(br.readLine());
-		// } catch (NumberFormatException | IOException e) {
-		// e.printStackTrace();
-		// }
-		// }
-		//
-		// // for (int i = 0; i < zeiten.length; i++) {
-		// // System.out.println(zeiten[i]);
-		// // }
-		// return zeiten;
-//		XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-		return zeiten;
 	}
+
 }
